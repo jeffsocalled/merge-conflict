0f847e0a2fd1af4b16e548cdfcf7dc1991ea7acc
G Praveen Kumar <84589796+jeffsocalled@users.noreply.github.com>
2023-10-25 23:33:28 +0530

    updated feature2

--- main.c
+++ main.c
@@ -5,19 +5,15 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
 feaature2()
 {
   //Sample Code for Feature 2
 }
-||||||| 8576620
-=======
+
 Initial Security()
 {
  //Sample Code
 }
-
->>>>>>> master
 
 
 }
